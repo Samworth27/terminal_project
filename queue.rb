82a80c5045c8ada8d4c7bce6d0781bdd4f51bd57
Sam Mitchell <Samworth27@gmail.com>
2022-03-23 16:40:04 +1000

    Class can be created and initialised. Tests for other features created

--- queue.rb
+++ queue.rb
@@ -1,5 +1,16 @@
+# frozen_string_literal: true
+
 # Queue Class
 class Queue
+  attr_reader :name, :description
+  attr_accessor :queue
+
+  def initialize(name, description)
+    @name = name
+    @description = description
+    @queue = []
+  end
+
   def exist?
     true
   end
