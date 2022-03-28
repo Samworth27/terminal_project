bd360bdacf956c0f49b2de00f1d7992a6c13c08b
Sam Mitchell <Samworth27@gmail.com>
2022-03-28 20:45:03 +1000

    + DB#Pretty() +DB Error Handling

--- lib/symptoms.rb
+++ lib/symptoms.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative 'symptoms/symptoms_db'
 
 # Init database
 symptoms = SymptomsDatabase.new
@@ -10,9 +11,23 @@ symptoms.fetch_data if gets.chomp == 'yes'
 puts 'Rebuild database [yes/no]'
 symptoms.reload_database if gets.chomp == 'yes'
 
-puts symptoms.fetch_by_id(1)
-puts symptoms.fetch_by_code('A')
-puts symptoms.fetch_by_id('root')
+# begin
+#   puts symptoms.fetch_by_id(9999)
+#   puts symptoms.fetch_by_code('AAAA')
+#   puts symptoms.fetch_by_id('root')
+# rescue PStore::Error=>e
+#   puts e
+# end
+
+def try
+  yield if block_given?
+rescue AppError => e
+  e
+end
+
+['A', 'AS01', 'AAAA', '#', 1, 9999].each do |i|
+  puts(try { symptoms.fetch_pretty(i) })
+end
 
 # test = Typhoeus::Request.new('https://browser.icpc-3.info/browse.php', params: { operation: 'getClasses', id: '#' })
 # test.on_complete do |response|
