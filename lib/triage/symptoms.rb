a6ec29c5f6ac8fa8d726f2546112fdb4547b1e4f
samworth27 <samworth27@gmail.com>
2022-03-30 12:47:18 +1000

    refactoring files

--- lib/triage/symptoms.rb
+++ lib/triage/symptoms.rb
@@ -1,13 +1,60 @@
-# frozen_string_literal: true
+# frozen_literal_string: true
 
 # Load all children
 Dir.glob(File.expand_path("../#{File.basename(__FILE__, ".*")}/*.rb", __FILE__)).each { |file| require_relative "#{File.basename(__FILE__, ".*")}/#{File.basename(file)}"}
 
+require 'tty-prompt'
 
-# Init database
-symptoms = SymptomsDatabase.new
+require_relative '../app_errors'
 
+# Api Reference
+# https://www.icpc-3.info/documents/extra/API-Calls.pdf
 
+# Structure used to fetch and store information IRT the ICPC
+# (Internation classification of Primary Care) using their own HTTP APIs
+class Symptoms
+  include DBBuild
+  include DBCalls
+  include DBBRowse
 
-# puts symptoms.browse
-puts symptoms
+  
+
+  # No need for any arguments
+  def initialize
+    @storage = PStore.new('./db/storage.pstore')
+    fetch_data if prompt?(main: 'Fetch new data?',help: 'Fetches data from the internet. May take a few minutes')
+    reload_database if prompt?(main: 'Update database from local JSON file', help: 'May take up to 20 seconds to complete')
+  end
+
+  def to_s
+    @storage.transaction do
+      "Database containing #{@storage[:size]} items"
+    end
+    
+  end
+
+  def codes  
+    @storage.transaction do
+      return @storage[:codes]
+    end
+  end
+
+  def reload_database
+    json = File.open('./db/.cached/symptoms.json')
+    json = json.read
+    json = JSON.parse(json, symbolize_names: true)
+    build(json[0])
+    print `clear`
+  end
+
+  def prompt?(main:, help: '')
+    prompt = TTY::Prompt.new
+    print `clear`
+    prompt.select(
+      main,
+      [{value: false, name:'no'},{value: true, name: 'yes'}],
+      help: help,
+      show_help: :always
+    )
+  end
+end
