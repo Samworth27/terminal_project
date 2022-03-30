0f5f54591ef0f8e37c814fbafb27d25a6be49020
Samworth27 <Samworth27@gmail.com>
2022-03-30 17:35:21 +1000

    Flags features complete
    All methods internal to flag class.
    Need to refactor and titivate

--- lib/flag_test.rb
+++ lib/flag_test.rb
@@ -1,29 +1,38 @@
-require_relative './lib/triage/flags'
+# Load all children
+require_relative './triage/flags'
+require_relative './triage/queue_object'
+require_relative './triage/symptoms'
+require_relative './triage/app_errors'
 
-Flags.new(['chest pain', 'fracture', 'hemorrhage','unconsciouse'])
-
-bad_test = Flags.new([:bad_1, :bad_2, :bad_3])
-
-
-
-
-def set_flags(flags_object)
-  loop do
-    print `clear`
-    puts "flags: #{flags_object} => #{flags_object.to_i} \n"
-    puts "flag to set. #{flags_object.class.flags.keys.map{|i| i.to_s}} or [exit] to finish inputing"
-    print '> '
-    flag = gets.chomp
-    break if flag == 'exit'
-    puts "Currently '#{flags_object.public_send(flag)}' set to [true/ false]"
-    print '> '
-    flag += '_set'
-    set_to = gets.chomp == 'true'
-    flags_object.public_send(flag, set_to)
-  end
+if ARGV.include?('--admin')
+  access = :admin
+else
+  access = :user
 end
 
-set_flags(Flags.new)
+flags = Flags.new(Symptoms.new(access).codes)
+
+# def set_flags(flags_object)
+#   symptoms = Symptoms.new(:user)
+#   prompt = TTY::Prompt.new
+#   loop do
+#     print `clear`
+#     puts "flags: #{flags_object} => #{flags_object.to_i} \n"
+#     # puts "flag to set. #{flags_object.class.flags.keys.map{|i| i.to_s}} or [exit] to finish inputing"
+#     # print '> '
+#     flag = symptoms.get_code
+#     return flags_object if flag == :exit
+#     puts flags_object
+#     set_to = prompt.select(
+#       "Currently '#{flags_object.public_send(flag)}' set to [true/ false]",
+#       [{value: true, name: 'True'},{value: false, name: "False"}])
+#     flag += '_set'
+#     flags_object.public_send(flag, set_to)
+#   end
+# end
+
+puts  flags.set_flags
+p flags.active_flags
 
 
 # p (0b0001^0b1111).to_s(2)
--- lib/triage/flags.rb
+++ lib/triage/flags.rb
@@ -6,26 +6,8 @@ Dir.glob(File.expand_path("../#{File.basename(__FILE__, ".*")}/*.rb", __FILE__))
 
 # Custom binary flag data type
 
-def flag_active(flags, item)
-  !(flags & item).zero?
-end
 
-def create_method(name, value)
-  # Returns true if the flag `name` is on
-  define_singleton_method(name) do
-    # puts "@value: #{@value} \n value: #{value}"
-    flag_active(@value, value)
-  end
 
-  # Input true or false to set flag
-  define_singleton_method("#{name}_set") do |set|
-    if set == true
-      @value |= value
-    elsif set == false
-      @value &= (value ^ (2**self.class.flags.size - 1))
-    end
-  end
-end
 
 class Flags
   @flags = {}
@@ -56,6 +38,39 @@ class Flags
     end
   end
 
+  def flag_active(flags, item)
+  !(flags & item).zero?
+  end
+
+  def create_method(name, value)
+    # Returns true if the flag `name` is on
+    define_singleton_method(name) do
+      # puts "@value: #{@value} \n value: #{value}"
+      flag_active(@value, value)
+    end
+
+    # Input true or false to set flag
+    define_singleton_method("#{name}_set") do |set|
+      if set == true
+        @value |= value
+      elsif set == false
+        @value &= (value ^ (2**self.class.flags.size - 1))
+      end
+    end
+  end
+
+  def set_flag(name, value)
+    if value == true
+        @value |= flag_value(name)
+      elsif set == false
+        @value &= (flag_value(name) ^ (2**self.class.flags.size - 1))
+      end
+  end
+
+  def flag?(code)
+    flag_active(@value, code)
+  end
+
   def flags
     self.class.flags
   end
@@ -78,5 +93,30 @@ class Flags
 
   def exists?
     true
+  end
+
+  def flag_value(code)
+    self.class.flags[code]
+  end
+
+  def active_flags
+    self.class.flags.filter {|k, v| flag_active(@value,v)}.keys
+  end
+
+  def set_flags
+    symptoms = Symptoms.new(:user)
+    prompt = TTY::Prompt.new
+    loop do
+      print `clear`
+      puts "flags: #{self.to_s} => #{self.to_i} \n"
+      # puts "flag to set. #{flags_object.class.flags.keys.map{|i| i.to_s}} or [exit] to finish inputing"
+      # print '> '
+      flag = symptoms.get_code
+      return active_flags if flag == :exit
+      set_to = prompt.select(
+        "Currently '#{flag?(flag_value(flag))}' set to [true/ false]",
+        [{value: true, name: 'True'},{value: false, name: "False"}])
+      set_flag(flag, set_to)
+    end
   end
 end
