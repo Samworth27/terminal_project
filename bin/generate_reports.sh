54349e9c16b392f84b59be5b37a804b3a5b709c6
samworth27 <samworth27@gmail.com>
2022-04-03 18:08:14 +1000

    new generate reports bash scripts

--- bin/generate_reports.sh
+++ bin/generate_reports.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 cd ..
+
 REPORT_PATH=./reports/$(date +%Y-%m-%d) bundle exec rspec --format RspecHtmlReporter ./spec
-git switch
+git switch gh-pages
+git commit ./reports
+git push
+git switch main
