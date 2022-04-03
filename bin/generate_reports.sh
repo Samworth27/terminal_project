c738369b72cb21f4f14e8e303ba3559182f3f13c
samworth27 <samworth27@gmail.com>
2022-04-03 18:31:06 +1000

    testing

--- bin/generate_reports.sh
+++ bin/generate_reports.sh
@@ -8,10 +8,8 @@ echo "Generating Reports"
 REPORT_PATH=./reports bundle exec rspec --format RspecHtmlReporter ./spec
 echo "Swapping branches"
 git switch gh-pages
-pwd
-sleep 
 echo "Committing reports to git"
 git add ./reports
-git commit
+git commit -m "automatic update of reports"
 git push
 git switch main
