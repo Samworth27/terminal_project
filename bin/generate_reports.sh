f76c07dcad9f5f496aaf3ee68b1491424cf6624c
samworth27 <samworth27@gmail.com>
2022-04-03 18:18:10 +1000

    commiting before testing generate_reports.sh

--- bin/generate_reports.sh
+++ bin/generate_reports.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 cd ..
-
-REPORT_PATH=./reports/$(date +%Y-%m-%d) bundle exec rspec --format RspecHtmlReporter ./spec
-git switch gh-pages
-git commit ./reports
-git push
-git switch main
+pwd
+echo "Generating Reports"
+REPORT_PATH=./reports bundle exec rspec --format RspecHtmlReporter ./spec
+# echo "Swapping branches"
+# git switch gh-pages
+# git commit ./reports
+# git push
+# git switch main
